60ce70602e9e2590ef7482391a835d923c16ed72
jinyoung4478 <102174146+jinyoung4478@users.noreply.github.com>
2022-06-15 23:52:53 +0900

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import App from './App';
 import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react';
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import { darkTheme } from "./theme";
@@ -9,11 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <StrictMode>
-    <RecoilRoot>
-      <ThemeProvider theme={darkTheme}>
-        <App />
-      </ThemeProvider>
-    </RecoilRoot>
-  </StrictMode>
+  <RecoilRoot>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
 );
